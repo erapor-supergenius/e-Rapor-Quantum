ba9c075b99fc702fd9199f1b8a499abb7e2b1e2b
e-Rapor Cerdas <erapor49@gmail.com>
2025-10-28 17:54:47 +0900

    Update profil_sekolah.js

--- profil_sekolah.js
+++ profil_sekolah.js
@@ -5,7 +5,7 @@
  ****************************************************/
 
 /* ---------- WEBAPP_URL ---------- */
-const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwR-1WBxIhP6JnuGoLe_j8lxczr5NiM8K-D5qjWHaZiH8fMxa_qY4RzgiO47Eo71eZU9A/exec";
+const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbxKK-LY8pDA0MiPu0UvzR0Bivks4rzEtmE4xHfQH-IFq78x21lVQcGiswAko2t5hPTZ-A/exec";
 
 /* ---------- Helper ---------- */
 const _ = id => document.getElementById(id);
